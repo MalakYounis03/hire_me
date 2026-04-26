cb9d15db42535d5849084d9f29b339e23c7bb226
Heba Alkashef <heba7hamed@gmail.com>
2026-04-26 13:01:24 +0300

    feat: complete job seeker modules and navigation

---
+++ lib/app/core/utils/app_assets.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesApple
+  /// assets/images/apple.png
+  static const String imagesApple = "assets/images/apple.png";
+
+  /// Assets for imagesCompany
+  /// assets/images/company.png
+  static const String imagesCompany = "assets/images/company.png";
+
+  /// Assets for imagesFacebook
+  /// assets/images/facebook.png
+  static const String imagesFacebook = "assets/images/facebook.png";
+
+  /// Assets for imagesGoogle
+  /// assets/images/google.png
+  static const String imagesGoogle = "assets/images/google.png";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.png
+  static const String imagesLogo = "assets/images/logo.png";
+
+  /// Assets for imagesObjects
+  /// assets/images/objects.svg
+  static const String imagesObjects = "assets/images/objects.svg";
+
+  /// Assets for imagesOnboard1
+  /// assets/images/onboard1.png
+  static const String imagesOnboard1 = "assets/images/onboard1.png";
+
+  /// Assets for imagesOnboard2
+  /// assets/images/onboard2.png
+  static const String imagesOnboard2 = "assets/images/onboard2.png";
+
+  /// Assets for imagesOnboard3
+  /// assets/images/onboard3.png
+  static const String imagesOnboard3 = "assets/images/onboard3.png";
+
+  /// Assets for imagesOnboard4
+  /// assets/images/onboard4.png
+  static const String imagesOnboard4 = "assets/images/onboard4.png";
+
+  /// Assets for imagesUser
+  /// assets/images/user.png
+  static const String imagesUser = "assets/images/user.png";
+}
+
---
+++ lib/app/core/utils/app_color.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+abstract class AppColor {
+  static Color Kblack = const Color(0xff16182C);
+  static Color kblue = const Color(0xff0D47A1);
+  static Color greydark = const Color(0xff626262);
+  static Color greyLight = const Color(0xffA6A6A6);
+  static Color greyVeryLight = const Color(0xFFE3E3E3);
+  static Color kwhite = const Color(0xffFFFFFF);
+  static Color offWhite = const Color(0xffCBD6FF);
+  static Color Ewhite = const Color(0xffF1F4FF);
+  static Color Eblack = const Color(0xff000000);
+}
